8060994b4761befb24470dc960dda33d63a962f7
Caio Martinez <caiomartinezpfs@gmail.com>
2018-05-05 10:48:18 -0300

    Adicionado codigo encontrado na internet

--- OTM-Delta/Program.cs
+++ OTM-Delta/Program.cs
@@ -11,7 +11,8 @@ namespace OTM_Delta
         static void Main(string[] args)
         {
             Tentativa1();
-            //Fim
+            Console.ReadKey();
+            Tentativa2();
             Console.ReadKey();
         }
 
@@ -78,7 +79,259 @@ namespace OTM_Delta
 
             Console.ReadKey();
         }
+
+        public static void Tentativa2()
+        {
+            Graph G = new Graph(26);
+
+            G.AddEdge(new Edge(0, 1));
+
+            G.AddEdge(new Edge(1, 2));
+            G.AddEdge(new Edge(1, 13));
+
+            G.AddEdge(new Edge(2, 3));
+            G.AddEdge(new Edge(2, 6));
+
+            G.AddEdge(new Edge(3, 4));
+
+            G.AddEdge(new Edge(4, 5));
+            G.AddEdge(new Edge(4, 7));
+
+            G.AddEdge(new Edge(5, 16));
+
+            G.AddEdge(new Edge(6, 7));
+            G.AddEdge(new Edge(6, 8));
+
+            G.AddEdge(new Edge(7, 9));
+
+            G.AddEdge(new Edge(8, 9));
+            G.AddEdge(new Edge(8, 10));
+
+            G.AddEdge(new Edge(9, 11));
+
+            G.AddEdge(new Edge(10, 11));
+            G.AddEdge(new Edge(10, 14));
+
+            G.AddEdge(new Edge(11, 15));
+
+            G.AddEdge(new Edge(12, 13));
+
+            G.AddEdge(new Edge(13, 14));
+
+            G.AddEdge(new Edge(14, 15));
+
+            G.AddEdge(new Edge(15, 16));
+
+            G.AddEdge(new Edge(16, 18));
+
+            G.AddEdge(new Edge(17, 18));
+            G.AddEdge(new Edge(17, 20));
+
+            G.AddEdge(new Edge(18, 21));
+
+            G.AddEdge(new Edge(19, 20));
+            G.AddEdge(new Edge(19, 22));
+
+            G.AddEdge(new Edge(20, 21));
+
+            G.AddEdge(new Edge(21, 23));
+
+            G.AddEdge(new Edge(22, 23));
+            G.AddEdge(new Edge(22, 24));
+
+            G.AddEdge(new Edge(23, 25));
+
+            Console.WriteLine(G.toString());
+
+            HashSet<int> resultado = G.ObterCoberturaMinima();
+
+            foreach (int item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
     }
+
+    public class Edge
+    {
+        private readonly int _v;
+        private readonly int _w;
+
+        public Edge(int v, int w)
+        {
+            this._v = v;
+            this._w = w;
+        }
+
+        public int Source()
+        {
+            return _v;
+        }
+
+        public int Target(int vertex)
+        {
+            if (vertex == _v) return _w;
+            else if (vertex == _w) return _v;
+            else throw new Exception("Illegal endpoint");
+        }
+
+        public String toString()
+        {
+            return String.Format("{0:d}-{1:d}", _v, _w);
+        }
+    }
+
+    public class Graph
+    {
+        private readonly int _v;
+        private int _e;
+        private LinkedList<Edge>[] _adj;
+
+        public Graph(int V)
+        {
+            if (V < 0)
+                throw new Exception("Number of vertices in a Graph must be nonnegative");
+
+            this._v = V;
+
+            this._e = 0;
+
+            _adj = new LinkedList<Edge>[V];
+
+            for (int v = 0; v < V; v++)
+            {
+                _adj[v] = new LinkedList<Edge>();
+            }
+        }
+
+        public int V()
+        {
+            return _v;
+        }
+
+        public int E()
+        {
+            return _e;
+        }
+
+        public void AddEdge(Edge e)
+        {
+            int v = e.Source();
+            int w = e.Target(v);
+            _adj[v].AddFirst(e);
+            _adj[w].AddFirst(e);
+            _e++;
+        }
+
+        public IEnumerable<Edge> Adj(int v)
+        {
+            return _adj[v];
+        }
+
+        public IEnumerable<Edge> Edges()
+        {
+            LinkedList<Edge> list = new LinkedList<Edge>();
+
+            for (int v = 0; v < _v; v++)
+            {
+                int selfLoops = 0;
+
+                foreach (Edge e in Adj(v))
+                {
+                    if (e.Target(v) > v)
+                    {
+                        list.AddFirst(e);
+                    }
+                    else if (e.Target(v) == v)
+                    {
+                        if (selfLoops % 2 == 0)
+                            list.AddFirst(e);
+                        selfLoops++;
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public String toString()
+        {
+            String NEWLINE = Environment.NewLine;
+
+            StringBuilder s = new StringBuilder();
+
+            s.Append(_v + " " + _e + NEWLINE);
+
+            for (int v = 0; v < _v; v++)
+            {
+                s.Append(v + ": ");
+
+                foreach (Edge e in _adj[v])
+                {
+                    s.Append(e.toString() + "  ");
+                }
+
+                s.Append(NEWLINE);
+            }
+            return s.ToString();
+        }
+
+        public HashSet<int> ObterCoberturaMinima()
+        {
+            HashSet<int> resultado = new HashSet<int>();
+
+            foreach (Edge item in Edges())
+            {
+                if (!resultado.Contains(item.Source()) &&
+                    !resultado.Contains(item.Target(item.Source())))
+                {
+                    resultado.Add(item.Source());
+                    resultado.Add(item.Target(item.Source()));
+                }
+            }
+
+            return resultado;
+        }
+
+        public HashSet<int> ObterCoberturaMinima2()
+        {
+            HashSet<int> resultado = new HashSet<int>();
+
+            while (Edges().Count() > 0)
+            {
+                var item = Edges().ElementAt(0);
+
+                resultado.Add(item.Source());
+                resultado.Add(item.Target(item.Source()));
+
+                while (this.Adj(item.Source()).Count() > 0)
+                {
+                    this.Remove(this.Adj(item.Source()).ElementAt(0));
+                }
+
+                while (this.Adj(item.Target(item.Source())).Count() > 0)
+                {
+                    this.Remove(this.Adj(item.Target(item.Source())).ElementAt(0));
+                }
+            }
+
+            return resultado;
+        }
+
+        public void Remove(Edge edge)
+        {
+            try
+            {
+                _adj[edge.Source()].Remove(edge);
+                _adj[edge.Target(edge.Source())].Remove(edge);
+            }
+            catch { }
+        }
+
+    }
+
 
     class Grafo
     {
